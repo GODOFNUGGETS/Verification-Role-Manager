71ae03e4ee4d22143e236438551d0dc77fad021d
King Nugget <81257774+GODOFNUGGETS@users.noreply.github.com>
2025-07-25 17:58:09 +0100

    Update main.py

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ from discord.ext import commands, tasks
 intents = discord.Intents.default()
 intents.members = True
 intents.guilds = True
-intents.message_content = False
-intents.presences = False
+intents.message_content = True
+intents.presences = True
 
 bot = commands.Bot(command_prefix="!", intents=intents)
 
